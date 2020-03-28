a9554267529ada062a8afe1b918697f22cc24759
chenhailiang <958304849@qq.com>
2020-03-28 16:14:02 +0800

    0328

--- dadi-code/demo01/app.js
+++ dadi-code/demo01/app.js
@@ -4,9 +4,15 @@ const url = require('url')
 http.createServer((req, res) => {
     res.writeHead(200,{"Content-type":"text/html;charset='utf-8'"})
     res.write("<head><meta charset='UTF-8'></head>")
-    res.write('你好 node.js')
-
+    
     console.log(req.url)
 
+    if(req.url == '/favicon.ico'){
+    	var userInfo = url.parse(req.url,true)
+    	
+    }
+
     res.end()
-}).listen(3000)
+}).listen(3000,()=>{
+	console.log('listen at 3000')
+})
